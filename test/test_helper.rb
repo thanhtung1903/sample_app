9d321f07cce0edb33739cb40be8fffb34d8d3044
Huỳnh Thanh Tùng <thanhtungcnttk13@gmail.com>
2017-07-28 10:10:23 +0700

    basic-login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
 ENV['RAILS_ENV'] ||= 'test'
-.
-.
-.
 class ActiveSupport::TestCase
   fixtures :all
-  include ApplicationHelper
-  .
-  .
-  .
+
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
